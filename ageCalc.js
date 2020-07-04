6a64b72c6c07a57e021e55bed2ea472bf0db9152
Nikko B <ncbadoles@gmail.com>
2020-07-04 00:35:02 +0000

    calculate age function

---
+++ ageCalc.js
@@ -0,0 +1,7 @@
+let ageCalculator =(name, yearOfBirth, currentYear)=>{
+    age = currentYear - yearOfBirth ;
+
+    return name + " is " + age + " years old."
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
